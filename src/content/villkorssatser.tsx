e450757914e825c35c079cc621348d052831ca2a
Anna Nordmark <anna.nero@gmail.com>
2026-08-25 08:50:40 +0200

    Nytt försök med ljusare rosa

--- src/content/villkorssatser.tsx
+++ src/content/villkorssatser.tsx
@@ -143,7 +143,7 @@ if 6 <= klockslag < 9:
         <thead>
           <tr>
             <th>Med elif (effektivt)</th>
-            <th>Bara med if (ineffektivt)</th>
+            <th style={{ backgroundColor: '#FFE1E6' }}>Bara med if (ineffektivt)</th>
           </tr>
         </thead>
         <tbody>
@@ -158,7 +158,7 @@ else:
     pris = 30`}
               </pre>
             </td>
-            <td style={{ backgroundColor: 'lightpink' }}>
+            <td style={{ backgroundColor: '#FFE1E6' }}>
               <pre style={{ margin: 0 }}>
 {`if alder < 7:
     pris = 0
